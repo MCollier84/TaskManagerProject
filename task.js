d2341f6d30c5bdd98b3314aa106aa3bac85c9b4d
Maurice Collier <mauricecollier84@yahoo.com>
2021-10-19 15:41:30 -0400

    Added the feature for user to enter options in uppercase or lowercase. Also, added the remove and add feature.

--- task.js
+++ task.js
@@ -83,8 +83,14 @@ let showTasks = ``;
 // For storing the value of a new task
 let newTask;
 
+// For storing the number of the task to remove
+let num;
+
+// For storing the value of the removed task
+let removed;
+
 // Displays the menu for the user to select an option. Also, sets the user's response to the userInput variable
-let userInput = prompt(menu);
+let userInput = prompt(menu).toUpperCase();
 
 
 // Loops and continues to display the menu until the user ends/closes the Task Manager (AKA Until the user enters CLOSE)
@@ -115,10 +121,16 @@ while (userInput !== `CLOSE`){
 
     // Prompts the user to enter a new task and stores their response
     newTask = prompt(`Please enter the new task:`);
-      // Alerts the user that their new task has been added
+    
+    // Continues looping and prompting the user until something is entered (NOTE: If the user does NOT type anything and just clicks OK, then the value returned is an empty string)
+    while (newTask === ``){
+      newTask = prompt(`Please enter the new task:`);
+    }
+    
+    // Alerts the user that their new task has been added
     alert(`"${newTask}" has been added!`);
 
-    // Adds the user's entry as a new item at the end of the tasks array
+    // Adds the user's entry as a new item at the end of the tasks array with .push
     tasks.push(newTask);
 
     // if (userInput === "NEW"){
@@ -126,13 +138,39 @@ while (userInput !== `CLOSE`){
       //   }
     }
 
+
+
   if (userInput === "REMOVE"){
-      alert(`This is the REMOVE feature`);
+    // Concatenates each task/item in the tasks array to the showTasks string variable. Also, sets/displays a number for each task.
+    for(i = 0; i < tasks.length; i++){
+
+      // Adding 1 to i (AKA The index) so the number will start displaying at 1. Also, using \n to create a new line (AKA Line break)
+      showTasks += `${i + 1}: ${tasks[i]}\n`;
+    }
+
+    // Prompts the user to enter a number and stores their response to the num variable.
+    // Using \n to create a new line (AKA Line break)
+    // Subtracting 1 from the user's entry so that it matches the index of the item that the user wants to remove from the tasks array.
+    // Just like with ParseInt, JavaScript will attempt to convert a string into a number when you try to Subtract from it. So ParseInt is not necessary here.
+    num = prompt(`Please enter a number to remove:\n${showTasks}`) - 1;
+
+    // Removes the task/item selected by the user from the tasks array. Also, sets the task/item that was removed to the REMOVED variable (NOTE: Splice returns the value that is removed as an item(s) in an array)
+    removed = tasks.splice(num, 1);
+
+    // Alerts user with the task/item that has been removed (NOTE: Using index on the REMOVED variable here because splice returns the value that is removed as an item in an array)
+    alert(`"${removed[0]}" has been removed`);
+
+    // Sets the value of the showTasks string variable back to an empty string
+    showTasks = ``;
+
+
+
+      // alert(`This is the REMOVE feature`);
   }
   
   
     // Displays the menu again
-  userInput = prompt(menu);
+  userInput = prompt(menu).toUpperCase();
 }
 
 // Alerts the user that they have closed the program
