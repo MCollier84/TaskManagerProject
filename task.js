d1620f56b0ccaccc415776f55e39d3d3d8a9a79f
Maurice Collier <mauricecollier84@yahoo.com>
2021-10-19 21:01:32 -0400

    Added the final version of the Taskmanager project.  Added the remove and add a task part to the taskmanager.

--- task.js
+++ task.js
@@ -154,6 +154,17 @@ while (userInput !== `CLOSE`){
     // Just like with ParseInt, JavaScript will attempt to convert a string into a number when you try to Subtract from it. So ParseInt is not necessary here.
     num = prompt(`Please enter a number to remove:\n${showTasks}`) - 1;
 
+    // Loop that continues looping until the user enters a valid number
+    // Verifies the user entered a whole (AKA Not a decimal) that is one of the options in the prompt (AKA Within the number range from 1 to length of the tasks array)
+    while (Math.floor(num) !== num || num < 0 || num >= tasks.length || !num){
+
+      // Alerts the user that they have NOT entered a valid number
+      alert(`Not a valid entry`);
+
+      // Prompts the user again as well as attempts to subtract 1 from their response
+      num = prompt(`Please enter a number to remove:\n${showTasks}`) - 1;
+    }
+
     // Removes the task/item selected by the user from the tasks array. Also, sets the task/item that was removed to the REMOVED variable (NOTE: Splice returns the value that is removed as an item(s) in an array)
     removed = tasks.splice(num, 1);
 
